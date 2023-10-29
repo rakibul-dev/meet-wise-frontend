9a37892e811d2194cbdc1f3e0f126b56065cba9f
rakibul-dev <mdrakibul.dev@gmail.com>
2023-10-30 00:56:26 +0600

    user login page created

--- src/pages/UserLoginPage.jsx
+++ src/pages/UserLoginPage.jsx
@@ -1,5 +1,79 @@
+import { Box, Button, TextField, Typography, Grid } from "@mui/material";
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { loginUser } from "../Redux/Slices/UserSlice";
+import { useNavigate } from "react-router-dom";
 const UserLoginPage = () => {
-  return <div>UserLoginPage</div>;
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const [userName, setUserName] = useState();
+  const [password, setPassword] = useState();
+
+  const user = useSelector((state) => state.userSlice.user);
+
+  const handleLogin = async () => {
+    try {
+      dispatch(loginUser({ username: userName, password })).then(() => {
+        if (user) {
+          navigate("/");
+        } else {
+          console.log("cant navigate");
+          console.log(user);
+        }
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  return (
+    <Box
+      display="flex"
+      flexDirection="column"
+      gap={2}
+      justifyContent="center"
+      alignItems="center"
+      height="100vh"
+    >
+      <Box
+        display="flex"
+        flexDirection="column"
+        gap={2}
+        justifyContent="center"
+        alignItems="center"
+        width="40%"
+      >
+        <Typography variant="h4">Login</Typography>
+        <TextField
+          type="text"
+          label="user name"
+          fullWidth
+          onChange={(e) => {
+            setUserName(e.target.value);
+          }}
+          value={userName}
+        />
+        <TextField
+          type="password"
+          label="password"
+          fullWidth
+          onChange={(e) => {
+            setPassword(e.target.value);
+          }}
+          value={password}
+        />
+        <Button
+          variant="contained"
+          fullWidth
+          onClick={() => {
+            handleLogin();
+          }}
+        >
+          Login
+        </Button>
+      </Box>
+    </Box>
+  );
 };
 
 export default UserLoginPage;
