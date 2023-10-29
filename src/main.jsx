d6fb004f1b088ff6c725d40b4abf2e39bf925b3b
rakibul-dev <mdrakibul.dev@gmail.com>
2023-10-30 00:57:13 +0600

    redux provider added

--- src/main.jsx
+++ src/main.jsx
@@ -6,12 +6,16 @@ import { ThemeProvider } from "@mui/material/styles";
 import darkTheme from "./theme/DarkTheme/DarkTheme.js";
 import { CssBaseline } from "@mui/material";
 import lightTheme from "./theme/LightTheme/LightTheme.js";
+import { Provider } from "react-redux";
+import store from "./Redux/Store.js";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <>
-    <ThemeProvider theme={lightTheme}>
-      <App />
-      <CssBaseline />
-    </ThemeProvider>
+    <Provider store={store}>
+      <ThemeProvider theme={lightTheme}>
+        <App />
+        <CssBaseline />
+      </ThemeProvider>
+    </Provider>
   </>
 );
