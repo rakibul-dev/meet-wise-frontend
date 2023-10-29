dd13535ce6deeb8ac20ce3ecfc7d961439bb6ba3
rakibul-dev <mdrakibul.dev@gmail.com>
2023-10-29 17:05:05 +0600

    routes updated

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -1,12 +1,22 @@
 import { createBrowserRouter } from "react-router-dom";
 import MessagePage from "../pages/MessagePage";
-import HelloWorld from "../pages/HelloWorld";
+
 import ErrorNotFound from "../pages/ErrorNotFound";
+import UserRegisterPager from "../pages/UserRegisterPager";
+import UserLoginPage from "../pages/UserLoginPage";
 
 const Router = createBrowserRouter([
   {
     path: "/",
     element: <MessagePage />,
+  },
+  {
+    path: "/register",
+    element: <UserRegisterPager />,
+  },
+  {
+    path: "/login",
+    element: <UserLoginPage />,
   },
   { path: "*", element: <ErrorNotFound /> },
 ]);
