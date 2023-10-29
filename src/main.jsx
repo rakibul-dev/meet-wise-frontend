eeda843922b11995ef674c786e4b61a4e5c752c1
rakibul-dev <mdrakibul.dev@gmail.com>
2023-10-29 17:08:21 +0600

    main file updated

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import "./index.css";
+// import "./index.css";
 import { ThemeProvider } from "@mui/material/styles";
 import darkTheme from "./theme/DarkTheme/DarkTheme.js";
+import { CssBaseline } from "@mui/material";
+import lightTheme from "./theme/LightTheme/LightTheme.js";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <ThemeProvider>
-      <App theme={darkTheme} />
+  <>
+    <ThemeProvider theme={lightTheme}>
+      <App />
+      <CssBaseline />
     </ThemeProvider>
-  </React.StrictMode>
+  </>
 );
